49fc1ac91006c8029fdb407b32e034c9fe044cde
rishabhs-s <47741140+rishabhs-s@users.noreply.github.com>
2020-09-23 15:52:22 +0530

    Create inheritance.cpp

---
+++ classes/inheritance.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    class A{
+        public:
+        int x=19;
+        
+    };
+    class B{
+        private:
+        int y =20;
+        cout<<y;
+    };
+
+}
+
